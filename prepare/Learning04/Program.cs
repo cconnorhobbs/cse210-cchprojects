a2a1ff917ba8f1bb2798bda748a1f86dfb3535a6
cconnorhobbs <150186838+cconnorhobbs@users.noreply.github.com>
2025-10-28 20:17:22 -0600

    finished learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math1 = new MathAssignment("Connor Hobbs", "Fractions", "8", "1-10");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Connor Hobbs", "European History", "The Invention of the Crossiant");
+        Console.WriteLine(w1.GetSummary());
+         Console.WriteLine(w1.GetWritingInformation());
     }
 }
